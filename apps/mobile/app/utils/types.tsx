4a18ea8390fd8b8d8c4a61f392b5b71db679a8af
sean <mtwbp101@gmail.com>
2025-02-25 01:37:20 -0500

    del unnecessary files

--- apps/mobile/app/utils/types.tsx
+++ apps/mobile/app/utils/types.tsx
@@ -1,9 +1,2 @@
-type TMatch = {
-    team1: string,
-    team2: string,
-    moneyline1: string,
-    moneyline2: string,
-    spread1: string,
-    spread2: string,
-    total: "234.5",
+export type TMatch = {
 }
