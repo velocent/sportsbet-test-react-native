f00f875ae1710f3ed44268d3277d93574ef306ce
sean <mtwbp101@gmail.com>
2025-02-25 07:33:24 -0500

    fix db schema

--- apps/server/database/schema.sql
+++ apps/server/database/schema.sql
@@ -4,7 +4,7 @@ USE bet_db;
 CREATE TABLE IF NOT EXISTS bets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     userId VARCHAR(50) NOT NULL,
-    outcomes JSON NOT NULL,
+    outcomes VARCHAR(50) NOT NULL,
     wagerAmount DECIMAL(10,2) NOT NULL,
     currency ENUM('Coin', 'Cash') NOT NULL,
     status ENUM('pending', 'confirmed', 'cancelled') DEFAULT 'pending',
